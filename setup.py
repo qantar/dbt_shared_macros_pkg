4f39cf4a6bffc0a83c1a4c6904a8bf8c19ef1d7e
Zouhair Qantar <e-z.qantar@lean.sa>
2025-02-10 19:15:30 +0300

    Added a line to steup file for inclusidng sql files as part of packages

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+
 setup(
-    name="dbt_shared_macros",
-    version="1.0.0",
-    packages=find_packages(),
-    install_requires=[],  # No dependencies required for dbt macros
-    include_package_data=True,
-    description="Shared dbt macros for multiple projects",
+    name="dbt_shared_macros",  # The name of your package
+    version="1.0.0",           # Package version
+    packages=find_packages(),  # This will find all sub-packages in dbt_shared_macros/
+    install_requires=[],       # If you have any dependencies, list them here
+    include_package_data=True, # This will include the macros (SQL files) in the package
+    package_data={             # Ensure SQL files are included in the package
+        "dbt_shared_macros": ["macros/*.sql"],
+    },
+    description="Shared dbt macros for multiple projects",  # Brief description of the package
 )
